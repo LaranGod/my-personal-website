d4734f51110ca325599bf0d55c5640d9a411288d
LaranGod <miguel05amancio2000@gmail.com>
2021-08-20 15:25:16 -0300

    Add border radius to workComponents

--- pages/myJobs.js
+++ pages/myJobs.js
@@ -2,50 +2,114 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import OnStartMotion from "../components/onStartMotion";
+import HoverMotion from "../components/hoverMotion";
 import Image from "next/image";
 
 export default function MyJobs() {
   return (
-    <div className={utilStyles.fullContainerHome}>
-      <div className="container">
+    <div className={utilStyles.pageBackground}>
+      <Layout myJobs>
+        {/* <div className="container"> */}
         <Head>
           <title>My jobs</title>
           <link
             rel="preload"
-            href="/fonts/Arcadepix/ARCADEPI.ttf"
+            href="/fonts/Mandalore/mandalore.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Noto-Sans/NotoSans-Regular.ttf"
             as="font"
             crossOrigin=""
           />
         </Head>
 
         <main>
-          <div className={utilStyles.pageBackground}>
+          <OnStartMotion title>
+            <h1 className={utilStyles.headingPres}>
+              My <span className={utilStyles.myName}>Projects</span>.
+            </h1>
+          </OnStartMotion>
+
+          <OnStartMotion>
             <div className={utilStyles.worksComponentDiv}>
-              <img
+              {/* <img
                 priority
                 className={utilStyles.workComponent}
-                src="/images/WorkComponent.jpg"
-                height="10%"
-                width="20%"
+                src="/images/jobSection.png"
+          
+                width="30%"
               />
               <img
                 priority
                 className={utilStyles.workComponent}
-                src="/images/WorkComponent.jpg"
-                height="10%"
-                width="20%"
+                src="/images/jobSection.png"
+                
+                width="30%"
               />
               <img
                 priority
                 className={utilStyles.workComponent}
-                src="/images/WorkComponent.jpg"
-                height="10%"
-                width="20%"
-              />
+                src="/images/jobSection.png"
+         
+                width="30%"
+              /> */}
+
+              <div className={utilStyles.workComponent}>
+                <HoverMotion workImage>
+                  <a
+                    href="https://github.com/LaranGod/UploadMaterialForm"
+                    target="_blank"
+                  >
+                    <img
+                      className={utilStyles.jobImage}
+                      priority
+                      src="/images/Infocep.jpeg"
+                      height="auto"
+                      width="auto"
+                    />
+                  </a>
+                </HoverMotion>
+              </div>
+              <div className={utilStyles.workComponent}>
+                <HoverMotion workImage>
+                  <a
+                    href="https://dribbble.com/shots/15876555-Rent"
+                    target="_blank"
+                  >
+                    <img
+                      className={utilStyles.jobImage}
+                      priority
+                      src="/images/Material_Form.jpeg"
+                      height="auto"
+                      width="auto"
+                    />
+                  </a>
+                </HoverMotion>
+              </div>
+              <div className={utilStyles.workComponent}>
+                <HoverMotion workImage>
+                  <a
+                    href="https://dribbble.com/shots/15876555-Rent"
+                    target="_blank"
+                  >
+                    <img
+                      className={utilStyles.jobImage}
+                      priority
+                      src="/images/Rent_mobile.png"
+                      height="auto"
+                      width="auto"
+                    />
+                  </a>
+                </HoverMotion>
+              </div>
             </div>
-          </div>
+          </OnStartMotion>
         </main>
-      </div>
+        {/* </div> */}
+      </Layout>
     </div>
   );
 }
